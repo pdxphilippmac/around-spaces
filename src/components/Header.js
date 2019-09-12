41c3c1d643a05e55a79218a350205823eb4307f1
Philipp Maecke <p.maecke@gmx.de>
2019-09-12 09:50:59 +0200

    change style

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div className="header">
-      <h1>AroundSpaces(Header)</h1>
+      <h1>IceCream Yeah</h1>
     </div>
   );
 }
--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -3,9 +3,15 @@ import React from "react";
 function Restaurant() {
   return (
     <div className="restaurant">
-      <p>
-        <b>Restaurant</b>
-      </p>
+      <img
+        className="restaurant__img"
+        src="https://myrealdomain.com/images250_/icecream-cartoon-3.png"
+        alt="Description"
+      />
+      <div>
+        <h2>IceCream</h2>
+        <p>Good Ice Cream</p>
+      </div>
     </div>
   );
 }
